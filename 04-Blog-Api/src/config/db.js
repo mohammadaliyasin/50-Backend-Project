07e2dfb9db4a192ad44d472ccc50ffb5c7a37ad2
mohammadaliyasin <mohammmadaliyasin6@gmail.com>
2026-03-23 04:46:17 +0530

    6th

---
+++ 04-Blog-Api/src/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connectDB = async ()=>{
+    try{
+    console.log("MongDB Not Connected.");
+    await mongoose.connect(process.env.MONGO_URI); 
+    console.log("MongDB Connected.");
+    }catch(err){
+    console.log("Connection Error:",err);
+    process.exit(1);
+    }   
+}
+
+module.exports = connectDB
