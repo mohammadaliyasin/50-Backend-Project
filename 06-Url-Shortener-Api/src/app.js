8bb9ba7680c57450ffd0c4034947566a4c8919b1
mohammadaliyasin <mohammmadaliyasin6@gmail.com>
2026-03-31 08:57:25 +0530

    10th

--- 06-Url-Shortener-Api/src/app.js
+++ 06-Url-Shortener-Api/src/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const urlRoutes = require("./routes/url_routes");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use("/", urlRoutes);
 
-app.use((req, res) => {
+app.get((req, res) => {
   res.status(200).json("Welcome to URL Shortener API");
 });
+
 module.exports = app;
--- 06-Url-Shortener-Api/src/config/db.js
+++ 06-Url-Shortener-Api/src/config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
+    console.log("Connecting to MongoDB...");
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected.");
   } catch (error) {
